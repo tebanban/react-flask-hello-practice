5dd48db8ce3efd7bda273288025fd68fc7ad383d
tebanban <estebancalf@gmail.com>
2021-12-31 04:07:13 +0000

    Role model added

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Role
 from api.utils import generate_sitemap, APIException
 
 api = Blueprint('api', __name__)
@@ -13,6 +13,15 @@ def handle_hello():
 
     response_body = {
         "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
+    }
+
+    return jsonify(response_body), 200
+
+@api.route('/role', methods=['POST', 'GET'])
+def handle_role():
+
+    response_body = {
+        "message": "role for roles"
     }
 
     return jsonify(response_body), 200
